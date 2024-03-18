13c9fea6d7aa5cc2f821b5d07ca3d3799e10319a
Abdul Rauf Phansupkar <114131077+abdulr2004@users.noreply.github.com>
2024-03-18 16:35:47 +0400

    Update ContentRichText.jsx

--- frontend/src/pages/ContentRichText.jsx
+++ frontend/src/pages/ContentRichText.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
-import app  from '../config/firebase';
+import app  from '../config/Firebase';
 import { db, auth } from '../config/Firebase';
 import * as Y from 'yjs';
 import { FireProvider } from 'y-fire';
