89aaa8dd868327f144d728a940df82d056b4c265
Maulik Mohan Shetty <114130610+maulikshh@users.noreply.github.com>
2024-03-26 12:58:47 +0400

    Update Firebase.jsx

--- frontend/src/config/Firebase.jsx
+++ frontend/src/config/Firebase.jsx
@@ -10,13 +10,13 @@ import {getStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const app = firebase.initializeApp( {
-    apiKey: "AIzaSyBnXGt0U0knvlfAXZPIic48zeuuMc3Irw8",
-    authDomain: "test-ad540.firebaseapp.com",
-    projectId: "test-ad540",
-    storageBucket: "test-ad540.appspot.com",
-    messagingSenderId: "1006670365144",
-    appId: "1:1006670365144:web:0e5c061e1fec4aade9b2ad",
-    measurementId: "G-8D19P5SWTC"
+  apiKey: "AIzaSyDa3x9YP5vNwMkRwp7Sislm3PXMks3bXuw",
+  authDomain: "merge-6c879.firebaseapp.com",
+  projectId: "merge-6c879",
+  storageBucket: "merge-6c879.appspot.com",
+  messagingSenderId: "577094059196",
+  appId: "1:577094059196:web:37c2d6c8d31688b9ebfcb9",
+  measurementId: "G-G192QXZDWQ"
 })
 
 // Initialize Firebase
